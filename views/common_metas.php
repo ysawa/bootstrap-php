97078ca7ea827a138464ec3d0efeb75fad0811ff
Y Sawa <yoshihirosawa@gmail.com>
2012-11-09 03:06:18 +0900

    add: templates

---
+++ views/common_metas.php
@@ -0,0 +1,17 @@
+<meta charset="utf-8">
+<title><?= $title ?></title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="">
+<meta name="author" content="">
+
+<!-- Le styles -->
+<link href="./bootstrap/css/bootstrap.css" rel="stylesheet">
+<style type="text/css">
+  body {
+    padding-top: 60px;
+    padding-bottom: 40px;
+  }
+</style>
+<link href="./bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
+
+<? include dirname(__FILE__) . '/html5js.php' ?>
